9d0e7326654b43f117e664e642be70813f25f144
Aryan Sethi <aryan.rdps@gmail.com>
2023-03-28 15:52:50 +0530

    Added project data

---
+++ project.sql
@@ -0,0 +1,16 @@
+
+--PROJECT DATA
+
+INSERT ALL
+    INTO PROJECT VALUES('ProductX', 1, 'Bellaire', 5)
+    INTO PROJECT VALUES('ProductY', 2, 'Sugarland', 5)
+    INTO PROJECT VALUES('ProductZ', 3, 'Houston', 5)
+    INTO PROJECT VALUES('Computerization', 10, 'Stafford', 4)
+    INTO PROJECT VALUES('Reorganization', 20, 'Houston', 1)
+    INTO PROJECT VALUES('Newbenefits', 30, 'Stafford', 4)
+SELECT 1
+FROM DUAL;
+
+SELECT * FROM PROJECT;
+
+DESC PROJECT;
